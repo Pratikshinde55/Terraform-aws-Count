6c1a80729095ec3718c7bdb93458f872438bc2d7
Pratik Shinde <145910708+Pratikshinde55@users.noreply.github.com>
2024-03-30 17:59:55 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,18 @@
+#####for plugin ##########
+
+terraform {
+
+  required_providers {
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.42.0"
+
+    }
+  }
+}
+
+provider "aws" {
+  region  = "ap-south-1"
+  profile = "default"
+}
